0b09be3761450006ebd06f189ee7c29f635187e7
Darclander <lukedarclander@gmail.com>
2020-09-07 01:54:29 +0200

    Added game and fixed main
    Setup a game file which will control the game whilst main will control the game file. Better setup?

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 
+#include "SDL2/SDL.h"
+
 #include "gui.h"
 #include "controller.h"
 #include "rocket.h"
+#include "game.h"
 
-int main() {
+
+// Game *game = nullptr; // Will be used when Game has a constructor
+
+int main(int argc, char *argv[]) {
 
     std::cout << "Hello there";
+
+    const int FPS = 60; // Set fps for game
+    const int frameDelay = 1000 / FPS;
+
+    uint32_t startingTick;
+    int endTick;
+
+    while (true) { // Change condition later
+
+        startingTick = SDL_GetTicks();
+
+        // Do stuff
+
+        endTick = SDL_GetTicks() - startingTick;
+
+        if (frameDelay > endTick) {
+            SDL_Delay(frameDelay - endTick);
+        }
+
+    }
+
 
     return 0;
 }
